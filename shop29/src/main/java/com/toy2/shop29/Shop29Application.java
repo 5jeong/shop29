ac59ef44cc80f423bc15eee52340c33d5f972fe2
JMsuper <tjsqls2067@naver.com>
2024-08-07 11:48:53 +0900

    init

---
+++ shop29/src/main/java/com/toy2/shop29/Shop29Application.java
@@ -0,0 +1,13 @@
+package com.toy2.shop29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shop29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shop29Application.class, args);
+	}
+
+}
---
+++ shop29/src/test/java/com/toy2/shop29/Shop29ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toy2.shop29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Shop29ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
