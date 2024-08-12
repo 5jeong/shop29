ed0ccf693157a47ba08e920c4e77a05cb16b6a15
5jeong <wjdgns4385@naver.com>
2024-08-12 12:02:13 +0900

    refactor : 회원dto 필드 수정

--- shop29/src/main/java/com/toy2/shop29/users/domain/UserDto.java
+++ shop29/src/main/java/com/toy2/shop29/users/domain/UserDto.java
@@ -2,6 +2,7 @@ package com.toy2.shop29.users.domain;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,22 +10,23 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@EqualsAndHashCode
 public class UserDto {
-    private String userId;  // 로그인 ID
-    private String email;   // 이메일
+    private String userId; //로그인 ID
     private String password;  // 비밀번호
-//    private String passwordConfirm;  // 비밀번호 확인
     private String userName;  // 회원 이름
+    private String userRole; //회원 권한
     private String postalCode;  // 우편번호
     private String addressLine1;  // 상세 주소 1
     private String addressLine2;  // 상세 주소 2
     private String addressReference;  // 주소 참고 항목
+    private String email;   // 이메일
     private String phoneNumber;  // 전화번호
     private LocalDate birthDate;  // 생년월일
-    private int gender;  // 성별 (1: 남성, 0: 여성)
+    private Integer gender;  // 성별 (1: 남성, 0: 여성)
     private LocalDate registrationDate;  // 가입일자
     private LocalDate withdrawalDate;  // 탈퇴일자
-    private int loginFailureCount;  // 로그인 실패 횟수
+    private Integer loginFailureCount;  // 로그인 실패 횟수
     private LocalDateTime lockExpiryTime;  // 계정 잠금 만료시간
     private LocalDateTime lastLoginFailureTime;  // 마지막 로그인 실패 시간
 }
