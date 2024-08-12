528c153d89edf81b6ce0a5d258dd966ac00f2c35
5jeong <wjdgns4385@naver.com>
2024-08-12 10:58:44 +0900

    feat : 로그인 잠금 로직 쿼리문 작성

--- shop29/src/main/java/com/toy2/shop29/users/mapper/UserMapper.java
+++ shop29/src/main/java/com/toy2/shop29/users/mapper/UserMapper.java
@@ -8,13 +8,35 @@ import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
-    UserDto findById(String userId); // id로 회원 조회
-    UserDto findByEmail(String userId); // email로 회원 조회
-    UserDto findByPhoneNumber(String userId); // id로 회원 조회
-    int insertUser(UserRegisterDto userRegisterDto); // 회원가입
-    int deleteUser(String userId); // id로 회원 삭제
-    int updateUser(@Param("userId") String userId, @Param("userUpdateDto") UserUpdateDto userUpdateDto); //회원 수정
-    int userCount(); // 회원 갯수 세기
-    boolean exisetedUserId();
+    // id로 회원 조회
+    UserDto findById(String userId);
+
+    // email로 회원 조회
+    String findByEmail(String email);
+
+    // phoneNumber로 회원 조회
+    String findByPhoneNumber(String phoneNumber);
+
+    // 회원가입
+    int insertUser(UserRegisterDto userRegisterDto);
+
+    // id로 회원 삭제
+    int deleteUser(String userId);
+
+    //회원 수정
+    int updateUser(@Param("userId") String userId, @Param("userUpdateDto") UserUpdateDto userUpdateDto);
+
+    // 회원 갯수 세기
+    int userCount();
+
+    // 로그인 실패 횟수 증가
+    int increaseLoginFailureCount(String userId);
+
+    // 계정 잠금 시간 설정
+    int lockAccount(String userId);
+
+    // 로그인 성공 또는 계정 잠금이 풀린경우 잠금관련 컬럼 초기화
+    int resetLoginFailureCount(String userId);
+
 }
 
