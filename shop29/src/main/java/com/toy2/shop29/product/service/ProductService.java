0f1c02dbae40d4593aaa7cb50b2739f1772390bf
Ryu-Eun <ektl4m@gmail.com>
2024-08-13 17:27:44 +0900

    feat: 나머지 필요한거

--- shop29/src/main/java/com/toy2/shop29/product/service/ProductService.java
+++ shop29/src/main/java/com/toy2/shop29/product/service/ProductService.java
@@ -1,148 +1,63 @@
-//package com.toy2.shop29.product.service;
-//
-//import com.toy2.shop29.product.domain.ProductDto;
-//import org.apache.ibatis.session.SqlSession;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.List;
-//
-//@Service
-//public class ProductService {
-//
-//    @Autowired
-//    SqlSession sqlSession;
-//
-//    String namespace = "com.toy2.shop29.dao.ProductMapper.";
-//
-//    //1. product_id 넣어서 맞는 product 찾기
-//    public ProductDto SelectById(int product_id){
-//
-//    }
-//
-//    //2. product 수정하기
-//    public int UpdateProduct(ProductDto product){
-//
-//    }
-//
-//    //3. product_id 넣어서 맞는 product 삭제하기
-//    public int DeleteById(int product_id){
-//
-//    }
-//
-//    //4. product 수정하기
-//    public int UpdateProduct(ProductDto){
-//
-//    }
-//
-//
-//
-//
-//
-//
-//
-//    //필터+검색(상품조회페이지 좌측에 있는 부분)
-//    //5. product 가격, 색상, 브랜드, 무료배송 상품인지, 품절상품 인지, 할인상품인지 넣어서 그에 맞는 product 서치
-//    public ProductDto SearchProduct(int pice, int color_id, int brand_id, 품절상품, 할인중, 임시품절){
-//
-//    }
-//    //search condition 너무많으면 묶기 << 배열이나
-//
-//    //6. 모든 상품 페이징(ALL)
-//
-//    //7. 카테고리별로 페이징
-//
-//    //8. NEW 상품 페이징
-//
-//    //9. EXCLUSIVE 상품 페이징
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//    //위의 search필터는 인자들을 복합적으로 입력받는 것에 비해, 정렬은 단일 인자 기준으로 나오다 보니 하나하나 다 만들어 줘야되나??
-//    //10. 추천순으로 정렬
-//    public List<ProductDto> SortingByRecommend(int ProductRecommend){
-//
-//    }
-//
-//    //11. 가격 높은순으로 정렬
-//    public List<ProductDto> SortingByDescending(int price){
-//
-//    }
-//
-//
-//    //12. 가격 낮은순으로 정렬
-//    public List<ProductDto> SortingByAscending(int price){
-//
-//    }
-//
-//
-//    //13. 신상품 순으로 정렬
-//    public List<ProductDto> SortingByNew(int created_date){
-//
-//    }
-//
-//    //14. 리뷰 많은 순으로 정렬
-//    public List<ProductDto> SortingByReviewCnt(int product_review_cnt){
-//
-//    }
-//
-//    //15. 높은 할인순으로 정렬
-//    public List<ProductDto> SortingBySaleRatio(int sale_ratio){
-//
-//    }
-//
-//    //16. 좋아요 많은순으로 정렬
-//    public List<ProductDto> SortingByLike(int product_like_cnt){
-//
-//    }
-//
-//    //17. 판매량 순으로 정렬
-//    //결제가 성공적으로 이루어진 주문의 갯수를 가져와야 할 듯함
-//    public List<ProductDto> SortingBySaleCnt(int product_sale_cnt){
-//
-//    }
-//
-//
-//
-//
-//
-//
-//
-//    //18. 제품이 받은 총 좋아요 카운트
-//
-//    //19. 제품이 받은 별점 평균
-//
-//    //20. 제품이 받은 총 리뷰 카운터
-//
-//
-//
-//
-//
-//
-//
-//    //21. 제품 구매나 장바구니 넣을때 제품옵션을 선택 했는지 안했는지 체크
-//
-//
-//
-//
-//
-//
-//    //22. 제품 썸네일 이미지 변경
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//}
+package com.toy2.shop29.product.service;
+
+import com.toy2.shop29.product.domain.ProductDto;
+
+import java.util.List;
+import java.util.Map;
+
+public interface ProductService {
+    //1.새 상품 등록 (insert)
+    int addProduct(ProductDto productDto);
+
+    //2-1.특정 product_id에 해당하는 상품 정보 조회하여 반환 (select)
+    ProductDto read(Integer product_id);
+
+    //2-2.모든 상품 정보 조회해서 반환 (select)
+    List<ProductDto> getList();
+
+    //2-3.특정 페이지에 해당하는 상품리스트 조회해서 반환 (select)
+    List<ProductDto> getPage(Map map);
+
+    //3.상품 수정 (update)
+    int modify(ProductDto productDto);
+
+    //4. 주어진 product_id에 해당하는 상품 삭제 (delete)
+    int remove(Integer product_id);
+
+    //총 상품 개수
+    int getCount();
+
+    //특정 카테고리에 해당하는 상품들 조회
+    List<ProductDto> getProductsByCategory(int categoryId);
+
+    //특정 브랜드에 해당하는 상품들을 조회
+    List<ProductDto> getProductsByBrand(int brandId);
+
+    //특정 가격 범위 안에 있는 상품들을 조회
+    List<ProductDto> getProductsByPriceRange(int minPrice, int maxPrice);
+
+    //가격 내림차순으로 상품들 정렬
+    List<ProductDto> sortByPriceDesc(Map<String, Object> paramMap);
+
+    //가격 오름차순으로 상품들 정렬
+    List<ProductDto> sortByPriceAsc(Map<String, Object> paramMap);
+
+    //최신 상품순으로 정렬
+    List<ProductDto> sortByNewest(Map<String, Object> paramMap);
+
+    //할인율 높은순으로 정렬
+    List<ProductDto> sortByHighDiscount(Map<String, Object> paramMap);
+
+    //높은 별점순으로 정렬
+    List<ProductDto> sortedByRating(Map<String, Object> paramMap);
+
+
+//    //특정 중분류에 해당하는 모든 제품 개수 세기
+//    int getCountByMiddleCategory(Integer middleCategoryId);
+//
+//    //특정 중분류에 해당하는 모든 상품 페이징
+//    List<ProductDto> getPageByMiddleCategory(Map<String, Object> map);
+
+
+
+}
