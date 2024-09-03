53148ae8118947bcd883b4c1188ecb1e795004be
Ryu-Eun <ektl4m@gmail.com>
2024-09-03 20:16:57 +0900

    style: 동적 header 시도

---
+++ shop29/src/main/java/com/toy2/shop29/product/controller/HeaderControllerAdvice.java
@@ -0,0 +1,38 @@
+package com.toy2.shop29.product.controller;
+
+import com.toy2.shop29.product.domain.category.MajorCategoryDto;
+import com.toy2.shop29.product.domain.category.MajorMiddleDto;
+import com.toy2.shop29.product.service.category.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.ui.Model;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class HeaderControllerAdvice {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @ModelAttribute
+    public void addAttributes(Model model) {
+
+        // 모든 대분류 가져오기
+        List<MajorCategoryDto> majorCategories = categoryService.findAllMajors();
+
+        // 각 대분류에 속한 중분류를 매핑
+        Map<MajorCategoryDto, List<MajorMiddleDto>> majorWithMiddleCategories = new HashMap<>();
+        for (MajorCategoryDto major : majorCategories) {
+            List<MajorMiddleDto> majorMiddles = categoryService.findMiddleMajor(major.getMajorCategoryId());
+            majorWithMiddleCategories.put(major, majorMiddles);
+        }
+
+        // 모델에 데이터 추가
+        model.addAttribute("majorWithMiddleCategories", majorWithMiddleCategories);
+
+    }
+}
